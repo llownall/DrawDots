a8c73353fe98f97bb23a36b98c160b584bea2d99
Anton Polovnikov <toxa1996@inbox.ru>
2020-02-16 22:30:21 +0700

    Модели по мелочи

---
+++ DrawDots/Models/IElement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawDots.Models
+{
+    interface IElement
+    {
+        Pair<double, double> GetPos();
+
+        double GetAngle();
+    }
+}
